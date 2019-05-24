e24e1be9b0d22866c3ea714a1b49c3bedb986860
cam1708 <43985717+cam1708@users.noreply.github.com>
2019-05-24 13:50:50 +0200

    Add files via upload

---
+++ includes/class-layup-wc-gateway.php
@@ -0,0 +1,246 @@
+<?php
+
+class WC_Layup_Gateway extends WC_Payment_Gateway {
+
+     /**
+      * Class constructor
+      */
+     public function __construct() {
+        $this->version = WC_GATEWAY_LAYUP_VERSION;
+        $this->id = 'layup'; // payment gateway plugin ID
+        $this->icon = 'https://layup.co.za/img/logo-color.168d4abe.png'; // URL of the icon that will be displayed on checkout page near your gateway name
+        $this->has_fields = false; // in case of custom credit card form
+        $this->method_title = __( 'LayUp', 'layup-gateway' );
+        $this->method_description = 'Activate your payment plan with a small deposit and break down the total cost into more affordable monthly payments.'; // will be displayed on the options page
+        $this->available_countries  = array( 'ZA' );
+        $this->available_currencies = (array)apply_filters('layup_gateway_available_currencies', array( 'ZAR' ) );
+        
+        
+    
+        // gateways can support products, subscriptions, refunds, saved payment methods,
+        
+        $this->supports = array(
+            'products'
+        );
+    
+        // Method with all the options fields
+        $this->init_form_fields();
+    
+        // Load the settings.
+        $this->init_settings();
+        $this->title = $this->get_option( 'title' );
+        $this->description = $this->get_option( 'description' );
+        $this->enabled = $this->get_option( 'enabled' );
+        $this->max_end_date = $this->get_option( 'lu_max_end_date' ) + 1;
+        $this->min_end_date = $this->get_option( 'lu_min_end_date' );
+        $this->logo_url = $this->get_option( 'lu_logo_url' );
+        $this->testmode = 'yes' === $this->get_option( 'lu_testmode' );
+        $this->layup_dep = (int)$this->get_option( 'layup_dep' );
+        $this->api_key = $this->testmode ? "myApiKey" : $this->get_option( 'lu_api_key' );
+        $this->api_url = $this->testmode ? "https://sandbox-api.layup.co.za/v1/orders" : "https://api.layup.co.za/v1/orders";
+    
+        // This action hook saves the settings
+        add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+
+     }
+
+    /**
+      * Plugin admin options
+      */
+     public function init_form_fields(){
+
+        $this->form_fields = array(
+            'enabled' => array(
+                'title'       => 'Enable/Disable',
+                'label'       => 'Enable LayUp Gateway',
+                'type'        => 'checkbox',
+                'description' => '',
+                'default'     => 'no'
+            ),
+            'title' => array(
+                'title'       => 'Title',
+                'type'        => 'text',
+                'description' => 'This controls the title which the user sees during checkout.',
+                'default'     => 'LayUp',
+                'desc_tip'    => true,
+            ),
+            'description' => array(
+                'title'       => 'Description',
+                'type'        => 'textarea',
+                'description' => 'This controls the description which the user sees during checkout.',
+                'default'     => 'Activate your payment plan with a small deposit and break down the total cost into more affordable monthly payments.',
+            ),
+            'lu_testmode' => array(
+                'title'       => 'Test mode',
+                'label'       => 'Enable Test Mode',
+                'type'        => 'checkbox',
+                'description' => 'Place the payment gateway in test mode using test API keys.',
+                'default'     => 'yes',
+                'desc_tip'    => true,
+            ),
+            'layup_dep' => array(
+                'title'       => 'Deposit Amount',
+                'type'        => 'number',
+                'description' => 'The deposit amount as a percentage',
+                'default'     => '20'
+            ),
+            'lu_min_end_date' => array(
+                'title'       => 'Min months',
+                'type'        => 'number',
+                'description' => 'The minimum number of months a customer can choose to pay off an order',
+                'default'     => '1'
+            ),
+            'lu_max_end_date' => array(
+                'title'       => 'Max months',
+                'type'        => 'number',
+                'description' => 'The maximum number of months a customer can choose to pay off an order',
+                'default'     => '6'
+            ),
+            'lu_api_key' => array(
+                'title'       => 'Live API Key',
+                'type'        => 'password'
+            ),
+            'lu_logo_url' => array(
+                'title'       => 'Your website logo',
+                'type'        => 'url',
+                'description' => 'The logo of your website that will appear on an order on the LayUp website (recommended dimentions: 320px x 200px )',
+                'default'     => ''
+            ),
+        );
+
+     }
+     
+    /**
+     * Check if this gateway is enabled and available in the base currency being traded with.
+     */
+    public function is_valid_for_use() {
+        $is_available          = false;
+        $is_available_currency = in_array( get_woocommerce_currency(), $this->available_currencies );
+
+        if ( $is_available_currency && $this->merchant_id && $this->merchant_key ) {
+            $is_available = true;
+        }
+
+        return $is_available;
+    }
+
+    public function admin_options() {
+        if ( in_array( get_woocommerce_currency(), $this->available_currencies ) ) {
+            parent::admin_options();
+        } else {
+        ?>
+            <h3><?php _e( 'LayUp', 'layup-gateway' ); ?></h3>
+            <div class="inline error"><p><strong><?php _e( 'Gateway Disabled', 'layup-gateway' ); ?></strong> <?php /* translators: 1: a href link 2: closing href */ echo sprintf( __( 'Choose South African Rands as your store currency in %1$sGeneral Settings%2$s to enable the LayUp Gateway.', 'layup-gateway' ), '<a href="' . esc_url( admin_url( 'admin.php?page=wc-settings&tab=general' ) ) . '">', '</a>' ); ?></p></div>
+            <?php
+        }
+    }
+
+    /*
+     * Processing the order and redirecting to layup
+     */
+    public function process_payment( $order_id ) {
+
+        global $woocommerce;
+        $products = array();
+        $i = 0;
+        // we need it to get any order detailes
+        $order = wc_get_order( $order_id );
+        $unid = md5(uniqid($order_id, true));
+        $ref = substr($unid, 0, 10);
+        $blog_title = get_bloginfo();
+        $order_items = $order->get_items( array('line_item', 'fee', 'shipping') );
+
+        // Format and add min and max dates
+        $curr_date = date('c');
+        $min_date = date('c', strtotime("+" . $this->min_end_date . " months", strtotime($curr_date)));
+        $max_date = date('c', strtotime("+" . $this->max_end_date . " months", strtotime($curr_date)));
+
+        // Build product array
+        foreach( $order_items as $item_id => $order_item ) {
+            $product = $order_item->get_product();
+            $products[$i] = array(
+                'amount'=> (int)$order_item->get_total() * 100,
+                'link'=> get_permalink( $product->get_id() ),
+                'sku'=> $product->get_sku()
+            );
+            $i++;
+
+        }
+
+        // Build and send LayUp order request
+        $order_details = array(
+            'products'=> $products,
+            'endDateMax' => $max_date,
+            'endDateMin' => $min_date,
+            'state' => 'CANCELLED',
+            'depositPerc' => $this->layup_dep,
+            'reference' => $ref,
+            'name' => $blog_title.' #'.$order_id,
+            'imageUrl' => $this->logo_url,
+  
+        );
+        $headers = array(
+             'Content-Type' => 'application/json',
+             'apikey' => $this->api_key,
+        );
+        
+        $order_details_json = json_encode( $order_details , JSON_UNESCAPED_SLASHES );
+        $args = array(
+            'headers' => $headers,
+            'body' => $order_details_json
+            );
+           
+        $response = wp_remote_post( $this->api_url, $args);
+
+        if( !is_wp_error( $response ) ) {
+
+            $body = json_decode( $response['body'], true );
+    
+            // Check if order was created successfully 
+            if ( $body['state'] == 'PARTIAL' ) {
+
+                // Link LayUp Order to Woocommerce order
+                update_post_meta( $order_id, 'layup_order_id', $body['_id'] );
+                update_post_meta( $order_id, 'layup_order_ref', $body['reference'] );
+                
+                // some notes added to Woocommerce order on admin dashboard
+                $order->update_status('wc-partial', __('Order created with LayUp', 'layup-gateway'));
+    
+                //reduce stock and empty cart
+                wc_reduce_stock_levels($order_id);
+			    $woocommerce->cart->empty_cart();
+
+               // Redirect to LayUp
+               return array(
+                   'result' => 'success',
+                   'redirect' => 'https://sandbox.layup.co.za/order/'. $body['_id']
+               );
+    
+            } else {
+               wc_add_notice( 'Error, please try again.', 'error' );
+               
+               return;
+           }
+    
+       } else {
+           wc_add_notice(  'Connection error.', 'error' );
+           return;
+       }
+
+     }
+
+    /**
+	*  Show possible admin notices
+	*/
+
+     public function admin_notices() {
+		if ( 'yes' !== $this->get_option( 'enabled' )
+			|| ! empty( $this->logo_url) ) {
+			return;
+		}
+
+		echo '<div class="error"><p>'
+			. __( 'LayUp requires additional configuration to function correctly.', 'layup-gateway' )
+			. '</p></div>';
+	}
+ }
---
+++ includes/layup-wc-functions.php
@@ -0,0 +1,154 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'register_layup_style' );
+
+function register_layup_style() {
+    wp_register_style("layup_css", plugins_url( '../css/payment-plans.css', __FILE__ ), array(), '1.0.0', 'all' );
+}
+
+/**
+ * Create the disable LayUp checkbox field on product admin page
+ */
+function create_layup_disable_field() {
+    $args = array(
+    'id' => 'layup_disable',
+    'label' => __( 'Disable LayUp checkout', 'layup-gateway' ),
+    'class' => 'lu-layup-disable',
+    'desc_tip' => true,
+    'description' => __( 'Check this box if you don`t want customers to be able to checkout using the LayUp gateway with this product in their cart.', 'layup-gateway' ),
+    );
+    woocommerce_wp_checkbox( $args );
+   }
+add_action( 'woocommerce_product_options_general_product_data', 'create_layup_disable_field' );
+
+/**
+ * Save the Disable LayUp field
+ */
+function save_layup_disable_field( $post_id ) {
+    $product = wc_get_product( $post_id );
+    $title = isset( $_POST['layup_disable'] ) ? $_POST['layup_disable'] : '';
+    $product->update_meta_data( 'layup_disable', sanitize_text_field( $title ) );
+    $product->save();
+}
+   add_action( 'woocommerce_process_product_meta', 'save_layup_disable_field' );
+
+/**
+ * Check products in cart for Disable LayUp field and disable LayUp gateway
+ */
+function check_layup_disable_field($gateways){
+
+    global $woocommerce;
+
+    $inarray = false;
+    
+    foreach ($woocommerce->cart->cart_contents as $key => $values ) { //enumerate over all cart contents
+        $layup_disable_meta = get_post_meta($values['product_id'], 'layup_disable', true);
+        if ( !empty( $layup_disable_meta ) ){
+            $inarray = true;//set inarray to true
+            break;
+        }
+    }
+
+    if($inarray) { //product is in the cart
+        unset($gateways['layup']);
+    } 
+    return $gateways;
+}
+add_filter('woocommerce_available_payment_gateways','check_layup_disable_field',1);
+
+/**
+ * Display LayUp icon on single product page
+ */
+function layup_display_icon() {
+    global $post;
+    // Check for the Disable LayUp field value
+    $product = wc_get_product( $post->ID );
+    $layup_disable_meta = $product->get_meta( 'layup_disable' );
+    if( $layup_disable_meta != 'yes' ) {
+    // Only display LayUp icon if Disable LayUp field is checked
+    echo '<div disabled style="font-size: 10px;padding: 10px 20px;margin-bottom: 15px;background-color: #fff;box-shadow: 0 0 13px #d6d6d6;-moz-box-shadow: 0 0 13px #d6d6d6;-webkit-box-shadow: 0 0 13px #d6d6d6;color: #2c3e50;border-radius: 150px; max-width: 50%;text-align: center;" class="btn-layup">
+    PAY WITH
+    <img style="width: 60px; vertical-align: middle; border-style: none" src="https://layup.co.za/img/logo-color.168d4abe.png">
+    </div>';
+    }
+   }
+   add_action( 'woocommerce_before_add_to_cart_button', 'layup_display_icon' );
+
+
+ // register WC Order status Partial and Placed
+function register_layup_order_statuses() {
+    register_post_status( 'wc-partial', array(
+        'label'                     => _x( 'Partial', 'Order status', 'woocommerce' ),
+        'public'                    => true,
+        'exclude_from_search'       => false,
+        'show_in_admin_all_list'    => true,
+        'show_in_admin_status_list' => true,
+        'label_count'               => _n_noop( 'Partial <span class="count">(%s)</span>', 'Partial <span class="count">(%s)</span>' )
+    ) );
+    register_post_status( 'wc-placed', array(
+        'label'                     => _x( 'Placed', 'Order status', 'woocommerce' ),
+        'public'                    => true,
+        'exclude_from_search'       => false,
+        'show_in_admin_all_list'    => true,
+        'show_in_admin_status_list' => true,
+        'label_count'               => _n_noop( 'Placed <span class="count">(%s)</span>', 'Placed <span class="count">(%s)</span>' )
+    ) );
+}
+
+add_action( 'init', 'register_layup_order_statuses' );
+// Add to list of WC Order statuses
+function add_layup_to_order_statuses( $order_statuses ) {
+    
+        // add new order statuses
+        $order_statuses['wc-partial'] = _x( 'Partial', 'Order status', 'woocommerce' );
+        $order_statuses['wc-placed'] = _x( 'Placed', 'Order status', 'woocommerce' );
+
+    return $order_statuses;
+}
+add_filter( 'wc_order_statuses', 'add_layup_to_order_statuses' );
+
+/**
+ *   Send Formatted Email @ WooCommerce "Placed" Order Status
+ */
+
+ // Adding action for 'Order Placed'
+add_filter( 'woocommerce_email_actions', 'placed_email_actions', 20, 1 );
+function placed_email_actions( $action ) {
+    $actions[] = 'woocommerce_order_status_wc-placed';
+    return $actions;
+}
+  
+add_action( 'woocommerce_order_status_changed', 'layup_status_custom_notification', 20, 4 );
+  
+function layup_status_custom_notification( $order_id, $old_status, $new_status, $order ) {
+    if ( $new_status == 'placed' ) {
+        $heading = 'Order Placed';
+        $subject = 'Order Placed and payment waiting to be completed';
+    
+        // Get WooCommerce email objects
+        $mailer = WC()->mailer()->get_emails();
+    
+        // Assign heading & subject to chosen object
+        $mailer['WC_Email_Customer_Processing_Order']->heading = $heading;
+        
+        $mailer['WC_Email_Customer_Processing_Order']->subject = $subject;
+        
+    
+        // Send the email with custom heading & subject
+        $mailer['WC_Email_Customer_Processing_Order']->trigger( $order_id );
+    
+        }
+    }
+
+
+/**
+ *   Add Content to the Customer Processing Order Email - WooCommerce
+ */
+ 
+add_action( 'woocommerce_email_before_order_table', 'layup_add_content_email', 20, 4 );
+ 
+function layup_add_content_email( $order, $sent_to_admin, $plain_text, $email ) {
+   if ( $email->id == 'customer_processing_order' && $order->get_status() == "placed" ) {
+      echo '<h2 class="email-upsell-title">Awaiting Full Payment</h2><p class="email-upsell-p">Thank you for placing an order using LayUp, You order will be monitored and as soon as full payment has been recieved, your goods wll be shipped.</p>';
+   }
+}
