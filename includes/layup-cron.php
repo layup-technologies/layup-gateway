b97d68fc21d7ea2e9057bbc8cdc5918cffda53cf
Cameron Morgan <dylanweb98@gmail.com>
2019-07-01 20:59:01 +0200

    Dates per product added
    New feature: Add multiple dates for each product that are selectable by customers. this affects the maximum payable date for the customers purchase.
    Dates that are higher or lower than the maximum and minimum dates in the gateway settings cannot be selected

--- includes/layup-cron.php
+++ includes/layup-cron.php
@@ -1,134 +1,134 @@
-<?php
-
-add_action('layup_order_check', 'check_payments');
-
-function check_payments() {
-
-
-    if (get_option( 'lu_testmode', true ) == 'yes') {
-         $api_key = "myApiKey";
-        $api_url = "https://sandbox-api.layup.co.za/";
-    } else {
-        $api_key = get_option( 'lu_api_key' );
-    $api_url = "https://api.layup.co.za/";
-        }
-        
-        
-        
-    $orders = get_posts( array(
-        'numberposts' => -1,
-        'post_type'   => wc_get_order_types(),
-        'post_status' => array('wc-partial', 'wc-placed')
-    ) );
-
-    if (empty($orders)) {
-
-        return;
-    
-    }
-
-    foreach( $orders as $order ) {
-        $order = wc_get_order( $order->ID );
-        $layup_order_id = get_post_meta( $order->get_order_number(), 'layup_order_id', true );
-        $headers = array(
-            'accept' => 'application/json',
-            'apikey' => $api_key,
-        );
-           
-        $order_args = array(
-            'headers' => $headers,
-               
-            );
-            
-        $order_response = wp_remote_get($api_url.'v1/orders/'.$layup_order_id.'?populate=plans,plans.payments', $order_args);
-
-
-        if( !is_wp_error( $order_response ) ) {
-
-            $body = json_decode( $order_response['body'], true );
-            
-            // Check payments 
-            if ( $body['state'] == 'PLACED' ) {
-
-                $pp=0;
-
-                // Save LayUp payment plans to Woocommerce order
-                foreach( $body['plans'] as $plans ) {
-                
-                update_post_meta( $order->get_order_number(), 'layup_pp_id_'.$pp, $plans['_id'] );
-                update_post_meta( $order->get_order_number(), 'layup_pp_months_'.$pp, $plans['months'] );
-
-                //get monthly amount
-                foreach( $plans['payments'] as $payment) {
-                    if ($payment['paid'] == false){
-                        $monthly = $payment['amount'];
-                        break;
-                    }
-                }
-
-                $amount = 0;
-                //get total due amount
-                foreach( $plans['payments'] as $payment) {
-                    if ($payment['paid'] == false){
-                        $amount = $amount + $payment['amount'];
-                    }
-                }
-                //convert cents to rands
-                $monthly_rands = $monthly/100;
-                $amount_rands = $amount/100; 
-                
-                //formate numbers to work with WC
-                $outstanding = number_format($amount_rands, 2, '.', '');
-                $monthly_payment = number_format($monthly_rands, 2, '.', '');
-
-                update_post_meta( $order->get_order_number(), 'layup_pp_outstanding_'.$pp, $outstanding );
-                update_post_meta( $order->get_order_number(), 'layup_pp_monthly_'.$pp, $monthly_payment );
-                $pp++;
-                
-                }
-                if ( $order->get_status() == "partial" ) {
-                $order->update_status('wc-placed', __('Deposit paid to LayUp', 'layup-gateway'));
-                }
-
-    
-            } elseif ( $body['state'] == 'COMPLETED' ) { // Check if paid in full
-               $order->update_status('processing', __('Order paid in full', 'layup-gateway'));
-
-               $pp=0;
-
-               foreach( $body['plans'] as $plans ) {
-                
-                update_post_meta( $order->get_order_number(), 'layup_pp_id_'.$pp, $plans['_id'] );
-                update_post_meta( $order->get_order_number(), 'layup_pp_months_'.$pp, $plans['months'] );
-
-                $pp++;
-                
-                }
-
-
-               return;
-           } elseif ( $body['state'] == 'CANCELLED' ) { // Check if cancelled and restore stock
-
-            foreach ($order->get_items() as $item_id => $item) {
-                // Get an instance of corresponding the WC_Product object
-                $product = $item->get_product();
-                $qty = $item->get_quantity(); // Get the item quantity
-                wc_update_product_stock($product, $qty, 'increase');
-            }
-            $order->update_status('cancelled', __('Order cancelled: '.$body['cancelledBy'], 'layup-gateway'));
-
-            return;
-             
-             
-        }
-
-    
-       } else {
-
-           return;
-       }
-    }
-
-
-	
-}
+<?php
+
+add_action('layup_order_check', 'check_payments');
+
+function check_payments() {
+
+
+    if (get_option( 'lu_testmode', true ) == 'yes') {
+         $api_key = "myApiKey";
+        $api_url = "https://sandbox-api.layup.co.za/";
+    } else {
+        $api_key = get_option( 'lu_api_key' );
+    $api_url = "https://api.layup.co.za/";
+        }
+        
+        
+        
+    $orders = get_posts( array(
+        'numberposts' => -1,
+        'post_type'   => wc_get_order_types(),
+        'post_status' => array('wc-partial', 'wc-placed')
+    ) );
+
+    if (empty($orders)) {
+
+        return;
+    
+    }
+
+    foreach( $orders as $order ) {
+        $order = wc_get_order( $order->ID );
+        $layup_order_id = get_post_meta( $order->get_order_number(), 'layup_order_id', true );
+        $headers = array(
+            'accept' => 'application/json',
+            'apikey' => $api_key,
+        );
+           
+        $order_args = array(
+            'headers' => $headers,
+               
+            );
+            
+        $order_response = wp_remote_get($api_url.'v1/orders/'.$layup_order_id.'?populate=plans,plans.payments', $order_args);
+
+
+        if( !is_wp_error( $order_response ) ) {
+
+            $body = json_decode( $order_response['body'], true );
+            
+            // Check payments 
+            if ( $body['state'] == 'PLACED' ) {
+
+                $pp=0;
+
+                // Save LayUp payment plans to Woocommerce order
+                foreach( $body['plans'] as $plans ) {
+                
+                update_post_meta( $order->get_order_number(), 'layup_pp_id_'.$pp, $plans['_id'] );
+                update_post_meta( $order->get_order_number(), 'layup_pp_months_'.$pp, $plans['months'] );
+
+                //get monthly amount
+                foreach( $plans['payments'] as $payment) {
+                    if ($payment['paid'] == false){
+                        $monthly = $payment['amount'];
+                        break;
+                    }
+                }
+
+                $amount = 0;
+                //get total due amount
+                foreach( $plans['payments'] as $payment) {
+                    if ($payment['paid'] == false){
+                        $amount = $amount + $payment['amount'];
+                    }
+                }
+                //convert cents to rands
+                $monthly_rands = $monthly/100;
+                $amount_rands = $amount/100; 
+                
+                //formate numbers to work with WC
+                $outstanding = number_format($amount_rands, 2, '.', '');
+                $monthly_payment = number_format($monthly_rands, 2, '.', '');
+
+                update_post_meta( $order->get_order_number(), 'layup_pp_outstanding_'.$pp, $outstanding );
+                update_post_meta( $order->get_order_number(), 'layup_pp_monthly_'.$pp, $monthly_payment );
+                $pp++;
+                
+                }
+                if ( $order->get_status() == "partial" ) {
+                $order->update_status('wc-placed', __('Deposit paid to LayUp', 'layup-gateway'));
+                }
+
+    
+            } elseif ( $body['state'] == 'COMPLETED' ) { // Check if paid in full
+               $order->update_status('processing', __('Order paid in full', 'layup-gateway'));
+
+               $pp=0;
+
+               foreach( $body['plans'] as $plans ) {
+                
+                update_post_meta( $order->get_order_number(), 'layup_pp_id_'.$pp, $plans['_id'] );
+                update_post_meta( $order->get_order_number(), 'layup_pp_months_'.$pp, $plans['months'] );
+
+                $pp++;
+                
+                }
+
+
+               return;
+           } elseif ( $body['state'] == 'CANCELLED' ) { // Check if cancelled and restore stock
+
+            foreach ($order->get_items() as $item_id => $item) {
+                // Get an instance of corresponding the WC_Product object
+                $product = $item->get_product();
+                $qty = $item->get_quantity(); // Get the item quantity
+                wc_update_product_stock($product, $qty, 'increase');
+            }
+            $order->update_status('cancelled', __('Order cancelled: '.$body['cancelledBy'], 'layup-gateway'));
+
+            return;
+             
+             
+        }
+
+    
+       } else {
+
+           return;
+       }
+    }
+
+
+	
+}
--- layup-gateway.php
+++ layup-gateway.php
@@ -1,96 +1,96 @@
-<?php
-/*
- * Plugin Name: WooCommerce LayUp Payment Gateway
- * Plugin URI: https://layup.co.za/how-it-works
- * Description: Activate your payment plan with a small deposit and break down the total cost into more affordable monthly payments.
- * Author: Cameron Morgan
- * Author URI: https://layup.co.za/
- * Version: 0.0.1
- *
-*/
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
- /*
- *  register WP cron event on activation
- */
-
-add_filter( 'cron_schedules', 'ten_add_cron_interval' );
- 
-function ten_add_cron_interval( $schedules ) {
-    $schedules['ten_mins'] = array(
-        'interval' => 600,
-        'display'  => esc_html__( 'Every Ten Minutes' ),
-    );
- 
-    return $schedules;
-}
-
-register_activation_hook(__FILE__, 'layup_activation');
-
-function layup_activation() {
-    if (! wp_next_scheduled ( 'layup_order_check' )) {
-	wp_schedule_event(time(), 'ten_mins', 'layup_order_check');
-    }
-}
-
- /*
- *  deregister WP cron event on deactivation
- */
-register_deactivation_hook(__FILE__, 'layup_deactivation');
-
-function layup_deactivation() {
-	wp_clear_scheduled_hook('layup_order_check');
-}
-
-require_once( plugin_basename( 'includes/layup-cron.php' ) );
-require_once( plugin_basename( 'includes/layup-wc-functions.php' ) );
-
- /*
- * This action hook registers our PHP class as a WooCommerce payment gateway
- */
-add_filter( 'woocommerce_payment_gateways', 'layup_add_gateway_class' );
-function layup_add_gateway_class( $gateways ) {
-	$gateways[] = 'WC_Layup_Gateway';
-	return $gateways;
-}
- 
-/*
- * The class itself from class-layup-wc-gateway.php, it is inside plugins_loaded action hook
- */
-add_action( 'plugins_loaded', 'layup_init_gateway_class' );
-function layup_init_gateway_class() {
-
-    if ( ! class_exists( 'WC_Payment_Gateway' ) ) {
-		return;
-	}
-
-    define( 'WC_GATEWAY_LAYUP_VERSION', '0.0.1' );
-
-    require_once( plugin_basename( 'includes/layup-payment-plan-shortcode.php' ) );
-    require_once( plugin_basename( 'includes/layup-payment-plan-tab.php' ) );
-    require_once( plugin_basename( 'includes/class-layup-wc-gateway.php' ) );
-
-   
-}
-
-/*
- * Plugin links on installed plugins page
- */
-function woocommerce_layup_plugin_links( $links ) {
-	$settings_url = add_query_arg(
-		array(
-			'page' => 'wc-settings',
-			'tab' => 'checkout',
-			'section' => 'wc_layup_gateway',
-		),
-		admin_url( 'admin.php' )
-	);
-
-	$plugin_links = array(
-		'<a href="' . esc_url( $settings_url ) . '">' . __( 'Settings', 'layup-gateway' ) . '</a>',
-		'<a href="https://layup.co.za/contact-us">' . __( 'Support', 'layup-gateway' ) . '</a>',
-		
-	);
-
-	return array_merge( $plugin_links, $links );
-}
+<?php
+/*
+ * Plugin Name: WooCommerce LayUp Payment Gateway
+ * Plugin URI: https://layup.co.za/how-it-works
+ * Description: Activate your payment plan with a small deposit and break down the total cost into more affordable monthly payments.
+ * Author: Cameron Morgan
+ * Author URI: https://layup.co.za/
+ * Version: 0.0.1
+ *
+*/
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+ /*
+ *  register WP cron event on activation
+ */
+
+add_filter( 'cron_schedules', 'ten_add_cron_interval' );
+ 
+function ten_add_cron_interval( $schedules ) {
+    $schedules['ten_mins'] = array(
+        'interval' => 600,
+        'display'  => esc_html__( 'Every Ten Minutes' ),
+    );
+ 
+    return $schedules;
+}
+
+register_activation_hook(__FILE__, 'layup_activation');
+
+function layup_activation() {
+    if (! wp_next_scheduled ( 'layup_order_check' )) {
+	wp_schedule_event(time(), 'ten_mins', 'layup_order_check');
+    }
+}
+
+ /*
+ *  deregister WP cron event on deactivation
+ */
+register_deactivation_hook(__FILE__, 'layup_deactivation');
+
+function layup_deactivation() {
+	wp_clear_scheduled_hook('layup_order_check');
+}
+
+require_once( plugin_basename( 'includes/layup-cron.php' ) );
+require_once( plugin_basename( 'includes/layup-wc-functions.php' ) );
+
+ /*
+ * This action hook registers our PHP class as a WooCommerce payment gateway
+ */
+add_filter( 'woocommerce_payment_gateways', 'layup_add_gateway_class' );
+function layup_add_gateway_class( $gateways ) {
+	$gateways[] = 'WC_Layup_Gateway';
+	return $gateways;
+}
+ 
+/*
+ * The class itself from class-layup-wc-gateway.php, it is inside plugins_loaded action hook
+ */
+add_action( 'plugins_loaded', 'layup_init_gateway_class' );
+function layup_init_gateway_class() {
+
+    if ( ! class_exists( 'WC_Payment_Gateway' ) ) {
+		return;
+	}
+
+    define( 'WC_GATEWAY_LAYUP_VERSION', '0.0.1' );
+
+    require_once( plugin_basename( 'includes/layup-payment-plan-shortcode.php' ) );
+    require_once( plugin_basename( 'includes/layup-payment-plan-tab.php' ) );
+    require_once( plugin_basename( 'includes/class-layup-wc-gateway.php' ) );
+
+   
+}
+
+/*
+ * Plugin links on installed plugins page
+ */
+function woocommerce_layup_plugin_links( $links ) {
+	$settings_url = add_query_arg(
+		array(
+			'page' => 'wc-settings',
+			'tab' => 'checkout',
+			'section' => 'wc_layup_gateway',
+		),
+		admin_url( 'admin.php' )
+	);
+
+	$plugin_links = array(
+		'<a href="' . esc_url( $settings_url ) . '">' . __( 'Settings', 'layup-gateway' ) . '</a>',
+		'<a href="https://layup.co.za/contact-us">' . __( 'Support', 'layup-gateway' ) . '</a>',
+		
+	);
+
+	return array_merge( $plugin_links, $links );
+}
 add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'woocommerce_layup_plugin_links' );
