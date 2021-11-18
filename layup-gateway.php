accfdece9a454237cf4399a0de4fc13ea4517ebd
CameronLayUp <80517347+CameronLayUp@users.noreply.github.com>
2021-11-18 14:47:16 +0200

    fixed error displaying on cart page

--- layup-gateway.php
+++ layup-gateway.php
@@ -12,7 +12,7 @@
 
  * Author URI: https://layup.co.za
 
- * Version: 1.7.14
+ * Version: 1.7.16
 
  *
 
@@ -116,7 +116,7 @@ function layup_init_gateway_class()
 
     }
 
-    define('WC_GATEWAY_LAYUP_VERSION', '1.7.15');
+    define('WC_GATEWAY_LAYUP_VERSION', '1.7.16');
 
     if (!wp_next_scheduled('layup_canceled_order_check'))
     {
