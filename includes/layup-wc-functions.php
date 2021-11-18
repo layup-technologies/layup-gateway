ac7ffa531285ab6a6d5e473d9dfe58ecf538d68e
CameronLayUp <80517347+CameronLayUp@users.noreply.github.com>
2021-11-18 22:19:47 +0200

    Update layup-wc-functions.php

--- includes/layup-wc-functions.php
+++ includes/layup-wc-functions.php
@@ -1223,7 +1223,7 @@ function layup_display_icon()
 						/* Stay in place */
 						z-index: 99999;
 						/* Sit on top */
-						padding-top: 150px;
+						padding-top: 100px;
 						/* Location of the box */
 						left: 0;
 						top: 0;
@@ -1281,7 +1281,7 @@ function layup_display_icon()
 						}
 		
 						.lumodal {
-							padding-top: 100px;
+							padding-top: 50px;
 						}
 					}
 		
