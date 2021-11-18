1b3ed4fc1a72f2e92495fb6e7f1296a1135cf45b
CameronLayUp <80517347+CameronLayUp@users.noreply.github.com>
2021-11-18 22:18:20 +0200

    Update layup-wc-functions.php

--- includes/layup-wc-functions.php
+++ includes/layup-wc-functions.php
@@ -1321,6 +1321,32 @@ function layup_display_icon()
    
                     jQuery("#lumyModal .lumodal-content .lupagecontent").load("https://layup.co.za/learn-more-popup/");
 
+                    // Get the modal
+					var modal = document.getElementById("lumyModal");
+		
+					// Get the button that opens the modal
+					var btn = document.getElementById("lumodallink");
+		
+					// Get the <span> element that closes the modal
+					var span = document.getElementsByClassName("luclose")[0];
+		
+					// When the user clicks the button, open the modal 
+					btn.onclick = function () {
+						modal.style.display = "block";
+					}
+		
+					// When the user clicks on <span> (x), close the modal
+					span.onclick = function () {
+						modal.style.display = "none";
+					}
+		
+					// When the user clicks anywhere outside of the modal, close it
+					window.onclick = function (event) {
+						if (event.target == modal) {
+							modal.style.display = "none";
+						}
+					}
+
                 });
 
 				jQuery( document ).ready(function() {
@@ -1378,31 +1404,6 @@ function layup_display_icon()
 
 				}
 
-					// Get the modal
-					var modal = document.getElementById("lumyModal");
-		
-					// Get the button that opens the modal
-					var btn = document.getElementById("lumodallink");
-		
-					// Get the <span> element that closes the modal
-					var span = document.getElementsByClassName("luclose")[0];
-		
-					// When the user clicks the button, open the modal 
-					btn.onclick = function () {
-						modal.style.display = "block";
-					}
-		
-					// When the user clicks on <span> (x), close the modal
-					span.onclick = function () {
-						modal.style.display = "none";
-					}
-		
-					// When the user clicks anywhere outside of the modal, close it
-					window.onclick = function (event) {
-						if (event.target == modal) {
-							modal.style.display = "none";
-						}
-					}
 				</script>';
             }
 
