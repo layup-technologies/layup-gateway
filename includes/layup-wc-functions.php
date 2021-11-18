eb1bae293b0c751dd53fd5fa267fd4bb02cbf183
CameronLayUp <80517347+CameronLayUp@users.noreply.github.com>
2021-11-19 00:03:56 +0200

    Update layup-wc-functions.php

--- includes/layup-wc-functions.php
+++ includes/layup-wc-functions.php
@@ -1339,14 +1339,12 @@ function layup_display_icon()
 					// When the user clicks on <span> (x), close the modal
 					span.onclick = function () {
 						modal.style.display = "none";
-                        jQuery("#layup-iframe").remove();
 					}
 		
 					// When the user clicks anywhere outside of the modal, close it
 					window.onclick = function (event) {
 						if (event.target == modal) {
 							modal.style.display = "none";
-                            jQuery("#layup-iframe").remove();
 						}
 					}
 
