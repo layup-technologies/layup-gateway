cbd47b0f5be3b80197de505b2b315460ef004bd3
CameronLayUp <80517347+CameronLayUp@users.noreply.github.com>
2022-01-26 00:21:13 +0200

    v1.7.19

--- layup-gateway.php
+++ layup-gateway.php
@@ -12,7 +12,7 @@
 
  * Author URI: https://layup.co.za
 
- * Version: 1.7.18
+ * Version: 1.7.19
 
  *
 
@@ -116,7 +116,7 @@ function layup_init_gateway_class()
 
     }
 
-    define('WC_GATEWAY_LAYUP_VERSION', '1.7.18');
+    define('WC_GATEWAY_LAYUP_VERSION', '1.7.19');
 
     if (!wp_next_scheduled('layup_canceled_order_check'))
     {
