fa10546b4439f9b77abdc7820747a534408fafde
CameronLayUp <80517347+CameronLayUp@users.noreply.github.com>
2022-03-15 03:12:04 +0200

    version bump 1.8.1

--- layup-gateway.php
+++ layup-gateway.php
@@ -12,7 +12,7 @@
 
  * Author URI: https://layup.co.za
 
- * Version: 1.8.0
+ * Version: 1.8.1
 
  *
 
@@ -95,7 +95,7 @@ function layup_init_gateway_class()
 
     }
 
-    define('WC_GATEWAY_LAYUP_VERSION', '1.8.0');
+    define('WC_GATEWAY_LAYUP_VERSION', '1.8.1');
 
     if (!wp_next_scheduled('layup_canceled_order_check'))
     {
