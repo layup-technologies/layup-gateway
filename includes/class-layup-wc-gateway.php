0ff51bab3c320792ea9b03942928418038348655
CameronLayUp <80517347+CameronLayUp@users.noreply.github.com>
2022-10-12 19:07:05 +0800

    Update class-layup-wc-gateway.php

--- includes/class-layup-wc-gateway.php
+++ includes/class-layup-wc-gateway.php
@@ -587,7 +587,7 @@ class WC_Layup_Gateway extends WC_Payment_Gateway
                 }
             }
             $check_dep_amount = array(array_sum($combine_amount));
-            file_put_contents("combine_test.txt", json_encode($combine_amount));
+            file_put_contents("combine_test.txt", array_sum($combine_amount));
             $check_dep_type = array("FLAT");
         }
 
