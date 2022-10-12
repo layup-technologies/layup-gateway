42887222721ff6aa30f6d5c0bc79f8284e1b0430
CameronLayUp <80517347+CameronLayUp@users.noreply.github.com>
2022-10-12 20:18:43 +0800

    Update layup-wc-functions.php

--- includes/layup-wc-functions.php
+++ includes/layup-wc-functions.php
@@ -1916,7 +1916,7 @@ inlineEditPost.edit = function( post_id ) {
                 foreach($cart_products as $combine_cart_item_id => $combine_cart_item) {
                     $combine_cart_product = $combine_cart_item['data'];
                     $combine_product_price = WC()->cart->get_product_price( $combine_cart_product );
-                    var_dump($combine_product_price);
+                    file_put_contents("test_combine_cart.txt", $combine_product_price);
                     if ($combine_cart_product->is_type('variation')) {
                         $combine_cart_product = wc_get_product($combine_cart_product->get_parent_id());
                     }
