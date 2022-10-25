48ba6f8c5d4549d493f0de06a3545f0e7982e800
CameronLayUp <80517347+CameronLayUp@users.noreply.github.com>
2022-10-25 20:28:40 +0800

    fixed woocommerce fee not being included

--- layup-gateway.php
+++ layup-gateway.php
@@ -12,7 +12,7 @@
 
  * Author URI: https://layup.co.za
 
- * Version: 1.9.1
+ * Version: 1.9.2
 
  *
 
@@ -94,7 +94,7 @@ function layup_init_gateway_class()
 
     }
 
-    define('WC_GATEWAY_LAYUP_VERSION', '1.9.1');
+    define('WC_GATEWAY_LAYUP_VERSION', '1.9.2');
 
     if (!wp_next_scheduled('layup_canceled_order_check'))
     {
