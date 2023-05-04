1ec94e5cc6fd830b367ce4f96d4bdb6e1e8874b2
CameronLayUp <80517347+CameronLayUp@users.noreply.github.com>
2023-05-05 00:37:08 +0800

    commented out refund webhook

--- includes/class-layup-wc-gateway.php
+++ includes/class-layup-wc-gateway.php
@@ -1170,7 +1170,7 @@ class WC_Layup_Gateway extends WC_Payment_Gateway
 
                 }
 
-            } elseif ($_POST['type'] == 'REFUNDPAYMENT'){
+            /* }  elseif ($_POST['type'] == 'REFUNDPAYMENT'){
                 $layup_payment_id = $_POST['body']['paymentId'];
                 
                 $headers = array(
@@ -1268,7 +1268,7 @@ class WC_Layup_Gateway extends WC_Payment_Gateway
                     );
                     $result = wc_create_refund($args);
                     
-                }
+                } */
             } elseif ($_POST['type'] == 'ORDERAMENDED'){
                 $layup_amendment_id = $_POST['body']['amendmentId'];
                 
